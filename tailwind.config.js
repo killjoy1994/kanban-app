024b6bda9bb8785bcfbf15ec6c1019b1b5cbf66f
Bagus Nugroho <papaobobo@gmail.com>
2023-07-02 16:43:29 +0700

    update modal ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "blue-violet": "#635fc7"
-      }
+        "blue-violet": "#635fc7",
+      },
+      
     },
   },
-  plugins: [require('daisyui'),],
-}
-
+  plugins: [require("daisyui"), require('tailwind-scrollbar')({ nocompatible: true })],
+};
